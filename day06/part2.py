aff08aaa6bad9ca8db84e2920cc582c9c5a0d1fe
Kyle Harris <kaharrs2@gmail.com>
2017-12-05 22:30:54 -0700

    Added day 6

---
+++ day06/part2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import sys
+import numpy as np
+
+
+def realloc(l):
+    idx = np.argmax(l)
+    val = np.max(l)
+    l[idx] = 0
+
+    for i in range(val):
+        l[(idx + i + 1) % len(l)] += 1
+
+    return l
+
+
+def memory_banks(in_file):
+    with open(in_file) as f:
+        in_string = f.read().strip()
+        banks = list(map(lambda x: int(x), in_string.split()))
+
+        cnt = 1
+        state_dict = {}
+        while True:
+            banks = realloc(banks)
+            tbanks = tuple(banks)
+            if tbanks in state_dict:
+                return cnt - state_dict[tbanks]
+            state_dict[tbanks] = cnt
+            cnt += 1
+
+
+if __name__ == '__main__':
+    output = memory_banks(sys.argv[1])
+    print('Output: {}'.format(output))
