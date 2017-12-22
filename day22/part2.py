97f330b46f530ca268297a9e12a7da086a1728ad
Kyle Harris <kaharrs2@gmail.com>
2017-12-21 22:55:01 -0700

    added day 22, still working on 20 and 21

---
+++ day22/part2.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+import sys
+
+
+class World():
+
+    def __init__(self, list_of_strings):
+        self.dict = {}
+        self.parse(list_of_strings)
+
+    def parse(self, list_of_strings):
+        mid = len(list_of_strings) // 2
+        for i, line in enumerate(list_of_strings):
+            for j, c in enumerate(line):
+                if c == '#':
+                    self.dict[(i - mid, j - mid)] = 1
+
+    def get_state(self, row, col):
+        if (row, col) not in self.dict:
+            return 0
+        else:
+            return self.dict[(row, col)]
+
+    def infect(self, row, col):
+        self.dict[(row, col)] = 1
+
+    def clean(self, row, col):
+        self.dict[(row, col)] = 0
+
+    def weaken(self, row, col):
+        self.dict[(row, col)] = 2
+
+    def flag(self, row, col):
+        self.dict[(row, col)] = 3
+
+
+class Virus():
+
+    def __init__(self):
+        self.row = 0
+        self.col = 0
+        self.facing = 'u'
+        self.num_infections = 0
+
+    def turn(self, world):
+        state = world.get_state(self.row, self.col)
+        if state == 1:
+            self.turn_right()
+            world.flag(self.row, self.col)
+        elif state == 0:
+            self.turn_left()
+            world.weaken(self.row, self.col)
+        elif state == 2:
+            world.infect(self.row, self.col)
+            self.num_infections += 1
+        elif state == 3:
+            self.turn_left()
+            self.turn_left()
+            world.clean(self.row, self.col)
+        else:
+            print(world.dict[self.row, self.col])
+            raise ValueError('WTF')
+
+        self.move_forward()
+
+    def turn_right(self):
+        if self.facing == 'u':
+            self.facing = 'r'
+        elif self.facing == 'r':
+            self.facing = 'd'
+        elif self.facing == 'd':
+            self.facing = 'l'
+        elif self.facing == 'l':
+            self.facing = 'u'
+
+    def turn_left(self):
+        if self.facing == 'u':
+            self.facing = 'l'
+        elif self.facing == 'r':
+            self.facing = 'u'
+        elif self.facing == 'd':
+            self.facing = 'r'
+        elif self.facing == 'l':
+            self.facing = 'd'
+
+    def move_forward(self):
+        if self.facing == 'u':
+            self.row -= 1
+        elif self.facing == 'r':
+            self.col += 1
+        elif self.facing == 'd':
+            self.row += 1
+        elif self.facing == 'l':
+            self.col -= 1
+
+
+def name(list_of_strings, N=10000):
+    world = World(list_of_strings)
+    virus = Virus()
+
+    for i in range(N):
+        virus.turn(world)
+
+    return virus.num_infections
+
+
+if __name__ == '__main__':
+    assert name(['..#', '#..', '...'], N=100) == 26
+    assert name(['..#', '#..', '...'], N=10000000) == 2511944
+
+    with open(sys.argv[1]) as f:
+        output = name([l.strip() for l in f.readlines()], N=10000000)
+        print('Output: {}'.format(output))
